98b2c82ce37f69e83653dc606366cb368bea1f83
rodrigo652 <rodrigo.camargo52@outlook.com>
2024-07-14 15:16:32 -0300

    iniciando projeto

---
+++ src/main/java/me/dio/SantanderDev2024Application.java
@@ -0,0 +1,14 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDev2024Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SantanderDev2024Application.class, args);
+    }
+
+}
---
+++ src/main/java/me/dio/controller/Pessoa.java
@@ -0,0 +1,6 @@
+package me.dio.controller;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+}
