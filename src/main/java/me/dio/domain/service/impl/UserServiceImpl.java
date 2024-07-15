ca9c314c64d605dde7b5dd4db8a9fabce12573cd
rodrigo652 <rodrigo.camargo52@outlook.com>
2024-07-14 21:48:26 -0300

    finalizando api

---
+++ src/main/java/me/dio/domain/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package me.dio.domain.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.domain.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User user) {
+        if(userRepository.existsByAccountNumber(user.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists!");
+        }
+        return null;
+    }
+}
